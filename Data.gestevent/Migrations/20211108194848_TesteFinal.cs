91701ae71ba717972b20e764acc21f38acda3e71
Bruno Cesar Franco <brunocesarfranco@gmail.com>
2021-11-15 18:46:54 -0300

    Migration Apresentação

---
+++ Data.gestevent/Migrations/20211108194848_TesteFinal.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Data.gestevent.Migrations
+{
+    public partial class TesteFinal : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ Data.gestevent/Migrations/20211108203200_Apresentacao.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Data.gestevent.Migrations
+{
+    public partial class Apresentacao : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
